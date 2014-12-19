375d0c2693606a2e141cbbd520f407f3e570d3a0
tksy2674 <tk.sy.vdesk2672@gmail.com>
2014-12-19 11:27:31 +0900

    Initial Commit

---
+++ Enmado/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Enmado
+//
+//  Created by 土橋臣吾 on 2014/12/19.
+//  Copyright (c) 2014年 土橋臣吾ゼミナール. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Enmado/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Enmado
+//
+//  Created by 土橋臣吾 on 2014/12/19.
+//  Copyright (c) 2014年 土橋臣吾ゼミナール. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
